3ea70fe94cdd44a6695c36ca37ddb336f245918f
gyezhov266 <gregory842@revature.net>
2022-12-20 18:19:16 -0500

    Added Scripting for SLIs

---
+++ logs/errorRate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# extract the lines containing the string "execution took" from the log file
+responses=$(grep "execution took" rollingFile.log | cut -d',' -f1 | cut -d'[' -f2 | cut -d' ' -f1)
+
+httpRequestTotal=0
+httpFailures=0
+
+# iterate through each extracted line
+for code in $responses
+do
+    ((httpRequestTotal++))
+    if [ $code -eq 500 ]
+    then
+        ((httpFailures++))
+    fi
+done
+
+httpSuccess=$(($httpRequestTotal - $httpFailures))
+
+result=$(echo "scale=2; $httpSuccess / $httpRequestTotal" | bc)
+
+echo "HTTP success rate: $result%"
--- logs/latency.sh
+++ logs/latency.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+# extract the lines containing the string "execution took" from the log file
+latency_lines=$(grep "execution took" rollingFile.log | cut -d',' -f2 | cut -d' ' -f4 | cut -d'.' -f1)
 
 # initialize total to 0
 total=0
+
 # initialize count to 0
 count=0
 
-# extract the lines containing the string "execution took" from the log file
-latency_lines=$(grep "execution took" rollingFile.log)
-
 # iterate through each extracted line
-for line in $latency_lines; do
+for line in $latency_lines
+do
   # extract the latency value from the line using cut
-  latency=$(echo $line | cut -d' ' -f5 | cut -d'.' -f1)
+  latency=$(echo $line)
   # add the latency value to the total
   total=$((total + latency))
   # increment the count
@@ -19,7 +20,7 @@ for line in $latency_lines; do
 done
 
 # calculate the average latency
-average=$((total / count))
+average=$(echo "scale=2; $total / $count" | bc)
 
 # print the average latency
 echo "Average latency: $average ms"
